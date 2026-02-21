93eef67bb6d63dd8414a4502f36c865ab7363bfd
Mia OHalloran <miaohall@bu.edu>
2026-02-20 23:19:31 -0500

    fix push

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-/*api using - https://api.disneyapi.dev/character*/
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
